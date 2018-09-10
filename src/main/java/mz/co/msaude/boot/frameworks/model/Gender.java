d7c255d1356053dd88fea3157e9a088a15d4a2ca
steliomo <steliomo@gmail.com>
2018-09-10 09:12:46 +0200

    Update Context and add Gender

---
+++ src/main/java/mz/co/msaude/boot/frameworks/model/Gender.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+package mz.co.msaude.boot.frameworks.model;
+
+/**
+ * @author Stélio Moiane
+ *
+ */
+public enum Gender {
+
+	MALE,
+
+	FEMALE
+}
--- src/main/java/mz/co/msaude/boot/frameworks/model/UserContext.java
+++ src/main/java/mz/co/msaude/boot/frameworks/model/UserContext.java
@@ -3,6 +3,13 @@
  */
 package mz.co.msaude.boot.frameworks.model;
 
+import java.time.LocalDate;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import br.com.six2six.bfgex.Gender;
+import mz.co.msaude.boot.frameworks.util.LocalDateAdapter;
+
 /**
  * @author Stélio Moiane
  *
@@ -11,7 +18,22 @@ public class UserContext {
 
 	private String uuid;
 
+	private String name;
+
+	private String surname;
+
+	private Gender gender;
+
+	@XmlJavaTypeAdapter(LocalDateAdapter.class)
+	private LocalDate dateOfBirth;
+
+	private String phoneNumber;
+
 	private String username;
+
+	private String password;
+
+	private String email;
 
 	public String getUuid() {
 		return this.uuid;
@@ -21,11 +43,67 @@ public class UserContext {
 		this.uuid = uuid;
 	}
 
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(final String surname) {
+		this.surname = surname;
+	}
+
+	public Gender getGender() {
+		return this.gender;
+	}
+
+	public void setGender(final Gender gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return this.dateOfBirth;
+	}
+
+	public void setDateOfBirth(final LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	public String getUsername() {
 		return this.username;
 	}
 
 	public void setUsername(final String username) {
 		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
 	}
 }
