3fb4344772890c46aacaf2c2e362b7b8cbf2f763
steliomo <steliom@gmail.com>
2021-09-20 18:28:14 +0200

    Fix rollback transaction

--- src/main/java/mz/co/msaude/boot/frameworks/service/AbstractService.java
+++ src/main/java/mz/co/msaude/boot/frameworks/service/AbstractService.java
@@ -5,11 +5,13 @@ package mz.co.msaude.boot.frameworks.service;
 
 import javax.transaction.Transactional;
 
+import mz.co.msaude.boot.frameworks.exception.BusinessException;
+
 /**
  * @author Stélio Moiane
  *
  */
-@Transactional
+@Transactional(rollbackOn = BusinessException.class)
 public abstract class AbstractService {
 
 }
