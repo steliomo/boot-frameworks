c3f8874ea2ba336bed7e162eff5df7ee5ea7dc06
steliomo <steliom@gmail.com>
2023-06-11 01:45:53 +0200

    Transaction manager TxType to SUPPORT

--- src/main/java/mz/co/msaude/boot/frameworks/service/AbstractService.java
+++ src/main/java/mz/co/msaude/boot/frameworks/service/AbstractService.java
@@ -4,6 +4,7 @@
 package mz.co.msaude.boot.frameworks.service;
 
 import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
 import mz.co.msaude.boot.frameworks.exception.BusinessException;
 
@@ -11,7 +12,7 @@ import mz.co.msaude.boot.frameworks.exception.BusinessException;
  * @author Stélio Moiane
  *
  */
-@Transactional(rollbackOn = BusinessException.class)
+@Transactional(rollbackOn = BusinessException.class, value = TxType.SUPPORTS)
 public abstract class AbstractService {
 
 }
